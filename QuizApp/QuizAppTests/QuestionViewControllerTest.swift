8ae86e4d2a7326a9a72a9e685a1b4ff587871dd4
Pavel Palancica <ppalancica@Pavels-MacBook-Pro.local>
2025-03-17 01:28:48 +0200

    Some refactoring

--- QuizApp/QuizAppTests/QuestionViewControllerTest.swift
+++ QuizApp/QuizAppTests/QuestionViewControllerTest.swift
@@ -11,37 +11,42 @@ import XCTest
 class QuestionViewControllerTest: XCTestCase {
     
     func test_viewDidLoad_rendersQuestionHeaderText() {
-        let sut = QuestionViewController(question: "Q1", options: [])
-        
-        _ = sut.view // Loads the view
-        
-        XCTAssertEqual(sut.headerLabel.text, "Q1")
+        XCTAssertEqual(makeSUT(question: "Q1").headerLabel.text, "Q1")
     }
+    
+    // MARK: These 2 methods can be deleted if we make the next test_viewDidLoad_renderOptions() method pass
     
     func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
-        let sut = QuestionViewController(question: "Q1", options: [])
-        
-        _ = sut.view
-        
-        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(options: []).tableView.numberOfRows(inSection: 0), 0)
     }
-    
+
     func test_viewDidLoad_withOneOption_rendersOneOption() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
-        
-        _ = sut.view
-        
+        let sut = makeSUT(options: ["A1"]) // NOTE: It should pass without 'sut' variable but it doesn't
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
     }
     
+    // MARK: General case (after combining previous 2 methods, and adding one more, but it didn't really work with SUT inline)
+    
+    func test_viewDidLoad_renderOptions() {
+        // XCTAssertEqual(makeSUT(options: []).tableView.numberOfRows(inSection: 0), 0)
+        // XCTAssertEqual(makeSUT(options: ["A1"]).tableView.numberOfRows(inSection: 0), 1)
+        // XCTAssertEqual(makeSUT(options: ["A1", "A2"]).tableView.numberOfRows(inSection: 0), 2)
+    }
+    
     func test_viewDidLoad_withOneOption_rendersOneOptionText() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
-        
-        _ = sut.view
+        let sut = makeSUT(options: ["A1"])
         
         let indexPath = IndexPath(row: 0, section: 0)
         let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
         
         XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+    
+    // MARK: Helpers
+    
+    func makeSUT(question: String = "", options: [String] = []) -> QuestionViewController {
+        let sut = QuestionViewController(question: question, options: options)
+        _ = sut.view // Loads the view
+        return sut
     }
 }
